026d7cdd0c058cc2c1b75d5c80bb06299812acc3
Vingrass <ingrassiav99@gmail.com>
2023-07-01 18:52:24 -0700

    added generatePassword function to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ function writePassword() {
   var PWlen = 0;
 
   while(PWlen < 8 || PWlen > 128){
-    PWlen = prompt("Please enter the desired length of the password (8 to 128):");
+    PWlen = parseInt(prompt("Please enter the desired length of the password (8 to 128):"));
     console.log(PWlen);
   }
 
@@ -17,21 +17,57 @@ function writePassword() {
 
   let incNum = confirm("Password can contain numbers?")
 
-  let incSped = confirm("Password can contain special characters?")
+  let incSpec = confirm("Password can contain special characters?")
 
-  if(!incLower && !incUpper && !incNum && !incSped){
+  if(!incLower && !incUpper && !incNum && !incSpec){
     alert("The password has to contain something, come on now.")
     return
   }
 
-  var password = generatePassword();
+  var password = generatePassword(PWlen, incLower, incUpper, incNum, incSpec);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+function generatePassword(PWlen, incLower, incUpper, incNum, incSpec){
 
+  var charPool = "";
+  var password = "";
+
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "1234567890"
+  var specChars = "~!@#$%^&*()-=+_;:[]{}|,.<>/?"
+
+  if(incLower){
+    charPool += lowerCase;
+  }
+  if(incUpper) {
+    charPool += upperCase;
+  }
+  if(incNum){
+    charPool += numericChars;
+  }
+  if(incSpec){
+    charPool += specChars; 
+  }
+
+  console.log(charPool);
+
+  var charPoolLen = charPool.length;
+  var randoNum = 0;
+
+  for(let i = 0; i < PWlen; i++){
+    randoNum = Math.floor(Math.random() * charPoolLen);
+    password += charPool[randoNum];
+  }
+
+
+  return password;
+
+}
 
 
 // Add event listener to generate button
