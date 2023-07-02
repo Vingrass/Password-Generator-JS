de5f65f828650518d639c5c2dfc23714fc535bb0
Vingrass <ingrassiav99@gmail.com>
2023-07-01 18:05:54 -0700

    added prompts for user input & variables to hold said input

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,25 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  let sign = prompt("Testicles, but pronounced like Heracles");
-  console.log(sign);
+  var PWlen = 0;
+
+  while(PWlen < 8 || PWlen > 128){
+    PWlen = prompt("Please enter the desired length of the password (8 to 128):");
+    console.log(PWlen);
+  }
+
+  let incLower = confirm("Password can contain lowercase letters?")
+
+  let incUpper = confirm("Password can contain uppercase letters?")
+
+  let incNum = confirm("Password can contain numbers?")
+
+  let incSped = confirm("Password can contain special characters?")
+
+  if(!incLower && !incUpper && !incNum && !incSped){
+    alert("The password has to contain something, come on now.")
+    return
+  }
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -18,9 +35,4 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);{
-
-
-
-
-};
+generateBtn.addEventListener("click", writePassword);
